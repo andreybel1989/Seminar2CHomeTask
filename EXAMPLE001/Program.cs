27a1cba4a9fd998e9c38c212dd7f4f1f48ce47b6
andreybel1989 <bav7269213@gmail.com>
2023-10-19 23:07:11 +0300

    First task

---
+++ EXAMPLE001/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("Введите трехзначное число: ");
+string number = Console.ReadLine();
+
+
+int TestNumber(string num)
+
+{    
+    if (num[0] == '-')
+    {
+        num = num.Substring(1);
+    }
+    int value;
+    bool isNumber = int.TryParse(num, out value);
+    
+    if (isNumber == false ||  num.Length != 3)
+    {
+        value = -1;
+    }
+    
+    return value;
+}
+
+int? CreatSecondNumber(int? number, string value)
+{
+    if(number != -1)
+    {
+        Console.Write($"Во втором рязряде числа {value} стоит значение равное ");
+        number = number/10%10;
+        
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели неверное число");
+        number = null;
+    }
+    
+    return number;
+}
+
+Console.WriteLine(CreatSecondNumber(TestNumber(number), number));
