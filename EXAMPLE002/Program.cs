96f0daef0085fd16a4313fc7a2dce83c5dce9d69
andreybel1989 <bav7269213@gmail.com>
2023-10-20 00:13:53 +0300

    Second task

---
+++ EXAMPLE002/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+string value = Console.ReadLine();
+
+int TestValue (string num)
+{
+    if (num[0] == '-')
+    {
+        num = num.Substring(1);
+    }
+    int value;
+    bool isNumber = int.TryParse(num, out value);
+    
+    if (isNumber == false )
+    {
+        value = -1;
+    }
+    
+    return value;
+}
+
+void CreateThridNumber(int resultTestValue)
+{
+    if (resultTestValue == -1)
+    {
+        Console.WriteLine("вы ввели не число");
+    }
+    
+    else if (resultTestValue < 100)
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
+    else
+    {
+        for(; resultTestValue >= 1000 ;)
+        {
+            resultTestValue /= 10;
+        }
+        Console.WriteLine($"третья цифра равна {resultTestValue % 10}");
+    }
+
+}
+CreateThridNumber(TestValue(value));
